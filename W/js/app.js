a1dc518b631ba66f7af2f3e74ee02aca7c58630b
Zach <zacbenincasa@gamil.com>
2022-07-21 19:32:35 -0600

    sorry this is so late

---
+++ W/js/app.js
@@ -0,0 +1,68 @@
+var tweets =[
+    tweet="this an example",
+    username="john117",
+    date_created= 11/12/22,
+    age=19,
+
+    tweet="this an example",
+    username="john117",
+    date_created= 11/12/22,
+    age=19,
+    
+    tweet="this an example",
+    username="john117",
+    date_created= 11/12/22,
+    age=19,
+
+    tweet="this an example",
+    username="john117",
+    date_created= 11/12/22,
+    age=19,
+
+    tweet="this an example",
+    username="john117",
+    date_created= 11/12/22,
+    age=19,
+
+    tweet="this an example",
+    username="john117",
+    date_created= 11/12/22,
+    age=19,
+
+    tweet="this an example",
+    username="john117",
+    date_created= 11/12/22,
+    age=19,
+
+    tweet="this an example",
+    username="john117",
+    date_created= 11/12/22,
+    age=19,
+
+    tweet="this an example",
+    username="john117",
+    date_created= 11/12/22,
+    age=19,
+
+    tweet="this an example",
+    username="john117",
+    date_created= 11/12/22,
+    age=19,
+
+    tweet="this an example",
+    username="john117",
+    date_created= 11/12/22,
+    age=19,
+
+]
+
+
+let counter = 0
+
+while (counter<tweet.length){
+    console.log(tweets[tweet],tweets[username],tweets[date_created],tweets[age]);
+    if (tweets[age]<18){
+        console.log("you fail good sir")
+    }
+    counter=counter+1
+}
